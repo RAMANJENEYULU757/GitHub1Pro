7a24c895e14bd7f2dab6cdac4fd4a2675f9b5f43
lenovo <lenovo@192.168.1.29>
2025-04-21 17:39:24 +0530

    First Commit...

---
+++ src/git/test10.java
@@ -0,0 +1,11 @@
+package git;
+
+public class test10 {
+
+	public static void main(String[] args) {
+		System.out.println("Ramanjeneyulu");
+		System.out.println("Ajay");
+		System.out.println("Bhaskar");
+	}
+
+}
